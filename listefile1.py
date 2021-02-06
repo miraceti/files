3420a5a18d4147747a5d0faf7b6e4e684278d52c
Rico Miraceti <elecam@gmail.com>
2021-02-06 19:03:27 +0100

    first commit

---
+++ listefile1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 11:33:21 2019
+
+@author: eric
+"""
+
+from os import listdir
+from os.path import isfile,join
+
+monrep = "d:\dl"
+fichiers = [f for f in listdir(monrep) if isfile(join(monrep,f))]
+print(fichiers)
---
+++ listefile2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 11:37:14 2019
+
+@author: eric
+"""
+
+from os import walk
+listefichier =[]
+for (repertoire, sousrepertoire, fichiers) in walk(monrep):
+    listefichier.extend(fichiers)
+    
+print(listefichier)python 
